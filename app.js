734fa8195e7eb5ad9d0ba1c2c6416575e6ac0a1e
Sobia51 <sobiasafdar51@gmail.com>
2022-10-07 16:14:40 +0500

    commiting files to the git

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+const mysql = require ('mysql');
+
+// connection with the database 
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+   database :'nodemysql'
+  });
+  
+  // connect 
+
+db.connect((err)=>{
+if(err)
+{
+    throw err ;
+}
+
+    console.log("db connected");
+
+  });
+
+const app = express ();
+
+// create DB 
+
+app.get('/createdb', (req, res)=>{
+db.query("CREATE DATABASE nodemysql", (err, result) =>{
+    if (err) throw err;
+    console.log(result);
+    res.send('Database created.....')
+  });
+});
+
+// create tables 
+
+app.get('/createpoststable', (req, res)=>{
+    let sql = "CREATE TABLE posts (id INT, name VARCHAR(255), age INT(3), city VARCHAR(255))";  
+    db.query(sql, (err, result) =>{
+        if (err) throw err;
+        console.log(result);
+        res.send('poststable created.....')
+      });
+    });
+    
+app.listen ('5000', ()=>
+{
+    console.log ("you are logged in ");
+});
